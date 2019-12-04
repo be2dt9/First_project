ed11857e5aecaa8a6231452ffc152094c0fa9980
TonyFarmer <56308827+TonyFarmer@users.noreply.github.com>
2019-12-05 01:55:33 +0200

    Update Formulas.py

--- Formulas.py
+++ Formulas.py
@@ -1,19 +1,27 @@
 import constmod as cm
-def arm(loadmass = cm.earthmass, unit = [1, 10], high = 0.001):
-    """
+def arm(loadmass = cm.earthmass, unit = ["mass", 60], high = 0.01, g = cm.g):
+    """ Функция рассчитывающая работу(A), перемещение(S), отношение плеч(ratio) и
+	время(t)(в зависимости от входных данных) по заданным аргументам: масса 
+	груза(load mass); единица измерения и ее значение(unit["", amount]);
+	высоту на которую необходимо поднять груз(high); гравитация(g).
     """
     if unit[0] == "mass":
-        F0 = loadmass * cm.g
-        F = unit[1] * cm.g
+        F0 = loadmass * g
+        F = unit[1] * g
         ratio = F0/F
         S = high * ratio
         A = S*F
-        t = ((2*S)**0.5)
-        print("Отношение плеч =", ratio, "Работа =", A, "Путь =", S, "Время =", t)
+        if (2*g*S)**0.5 > cm.c:
+            tacceleration = cm.c/g #время через которое точка достигнет скорости света
+            Sacceleration = tacceleration**2*g/ 2 #расстояние которое тело пройдет с ускорением
+            t = tacceleration + (S - Sacceleration)/cm.c
+        else:
+            t = (2*S/g)**0.5
+        print("Отношение плеч =", ratio, "Работа =", A, "Перемещение =", S, "Время =", t)
     else:
-        F0 = loadmass * cm.g
+        F0 = loadmass * g
         ratio = F0/unit[1]
         S = high * ratio
         A = S*unit[1]
-        print("Отношение плеч =", ratio, "Работа =", A, "Путь =", S)
+        print("Отношение плеч =", ratio, "Работа =", A, "Перемещение =", S)
 arm()
